a201173c76da9637bbea9629321d9d89b97e480e
TanithFlory <tanithflory7770@gmail.com>
2024-09-30 20:43:21 +0530

    Update: css

--- app/components/AddTrip/AddTrip.tsx
+++ app/components/AddTrip/AddTrip.tsx
@@ -9,10 +9,12 @@ export default function AddTrip({
   handleCloseModal: (e: CloseModalType) => void;
 }) {
   return (
-    <form className="bg-white px-[24px] pt-[20px] w-[648px] h-[392px] relative">
-      <h3 className="text-fs-20 font-bold mb-[36px]">Add Trip</h3>
-      <InputFields />
-      <div className="flex items-center gap-4 py-4 border-t-borderColor border-t-[1px] justify-end">
+    <form className="bg-white  pt-[20px] w-[648px] pb-[16px] h-[408px] relative rounded-[8px]">
+      <div className="px-[24px]">
+        <h3 className="text-fs-20 font-bold mb-[36px]">Add Trip</h3>
+        <InputFields />
+      </div>
+      <div className="flex items-center gap-4 py-4 border-t-borderColor border-t-[1px] pr-[32px] justify-end mb-4">
         <PrimaryButton
           text="Cancel"
           className="bg-white border-borderColor border-[1px] text-fs-12 max-w-[98px] text-black"
--- app/components/UpdateStatus/UpdateStatus.tsx
+++ app/components/UpdateStatus/UpdateStatus.tsx
@@ -9,10 +9,12 @@ export default function UpdateStatus({
   handleCloseModal: (e: CloseModalType) => void;
 }) {
   return (
-    <form className="bg-white px-[24px] pt-[20px] w-[328px] h-[312px] relative">
-      <h3 className="text-fs-20 font-bold mb-[36px]">Update status</h3>
-      <InputFields />
-      <div className="flex items-center gap-4 py-4 border-t-borderColor border-t-[1px] justify-end">
+    <form className="bg-white pt-[20px] pb-[16px] w-[328px] h-[322px] rounded-[8px] relative">
+      <div className=" px-[24px] ">
+        <h3 className="text-fs-20 font-bold mb-[36px]">Update status</h3>
+        <InputFields />
+      </div>
+      <div className="flex items-center gap-4 py-4 border-t-borderColor border-t-[1px] pr-[32px] justify-end">
         <PrimaryButton
           text="Cancel"
           className="bg-white border-borderColor border-[1px] text-fs-12 max-w-[98px] text-black"
